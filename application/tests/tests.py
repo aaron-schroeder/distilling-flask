fe7a98a599461684479b3803d63a7fc6d25c3dc8
aaron-schroeder <aaschro@gmail.com>
2022-11-17 16:38:28 -0700

    Ch 3 update: Make unittests pass

--- application/tests/tests.py
+++ application/tests/tests.py
@@ -14,39 +14,43 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 
 class HomePageTest(unittest.TestCase):
-  def setUp(self):
+  
+  @classmethod
+  def setUpClass(cls):
     """
     Refs:
       https://coddyschool.com/upload/Flask_Web_Development_Developing.pdf#page=221
       https://stackoverflow.com/questions/60111814/flask-application-was-not-able-to-create-a-url-adapter-for-request
     """
-    # self.app = create_app('testing')
-    self.app = create_app(test_config={
+    # cls.app = create_app('testing')
+    cls.app = create_app(test_config={
       'TESTING': True,
       'SQLALCHEMY_DATABASE_URI': os.environ.get(
         'TEST_DATABASE_URL',
         'sqlite:///' + os.path.join(BASEDIR, 'data-test.sqlite')
       )
     })
-    self.app_context = self.app.app_context()
-    self.app_context.push()
-    self.request_context = self.app.test_request_context()
-    self.request_context.push()
+    cls.app_context = cls.app.app_context()
+    cls.app_context.push()
+    cls.request_context = cls.app.test_request_context()
+    cls.request_context.push()
     # db.create_all()  # happens in `create_app()`
-    self.client = self.app.test_client(use_cookies=True)
+    cls.client = cls.app.test_client(use_cookies=True)
 
-  def tearDown(self):
+  @classmethod
+  def tearDownClass(cls):
     db.session.remove()
     db.drop_all()
-    self.app_context.pop()
-    self.request_context.pop()
+    cls.app_context.pop()
+    cls.request_context.pop()
 
   def test_root_url_resolves_to_home_page_view(self):
     result = url_for('start_dashapp')
     self.assertEqual(result, '/')
 
   def test_home_page_returns_correct_html(self):
-    response = self.client.get(url_for('start_dashapp'))
+    response = self.client.get('/')
+
     html = response.get_data(as_text=True)
     self.assertTrue(html.startswith('<!doctype html>'))
     self.assertIn('<title>Welcome - Training Zealot</title>', html)
