33f235d5ca5a499bc03c5d6a4f576b25d8034bcd
aaron-schroeder <aaschro@gmail.com>
2022-11-18 08:52:54 -0700

    REF: reusable func to check for link text

--- application/tests/functional_tests.py
+++ application/tests/functional_tests.py
@@ -37,6 +37,10 @@ class NewVisitorTest(unittest.TestCase):
   def tearDown(self):
     self.browser.quit()
 
+  def check_for_link_text(self, link_text):
+    self.assertIsNotNone(
+      self.browser.find_element(By.LINK_TEXT, link_text))
+
   def test_can_see_landing_page(self):
 
     # Edith has heard about a cool new online to-do app. She goes
@@ -58,17 +62,13 @@ class NewVisitorTest(unittest.TestCase):
     self.assertEqual(navbar.get_attribute('href'), urljoin(BASE_URL, '/'))
 
     # She sees links inviting her to visit a list of her Strava activities...
-    self.assertIsNotNone(
-      self.browser.find_element(By.LINK_TEXT, 'Strava activities'))
+    self.check_for_link_text('Strava activities')
 
     # ...a training log dashboard...
-    self.assertIsNotNone(
-      self.browser.find_element(By.LINK_TEXT, 'Training log dashboard'))
+    self.check_for_link_text('Training log dashboard')
 
     # ...and a file analysis dashboard.
-    self.assertIsNotNone(
-      self.browser.find_element(By.LINK_TEXT,
-        'Analyze an activity file (.gpx, .fit, .tcx, .csv)'))
+    self.check_for_link_text('Analyze an activity file (.gpx, .fit, .tcx, .csv)')
 
     self.fail('Finish the test!')
 
