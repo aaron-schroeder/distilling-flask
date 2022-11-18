73eefd050fe1a7bec7ba0ba6915961b41b0ac359
aaron-schroeder <aaschro@gmail.com>
2022-11-17 18:25:50 -0700

    Ch 4: - Functional test now checks that header and expected links are present - home page view already used a template

--- application/tests/functional_tests.py
+++ application/tests/functional_tests.py
@@ -8,6 +8,7 @@ import unittest
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -32,7 +33,7 @@ class NewVisitorTest(unittest.TestCase):
   def tearDown(self):
     self.browser.quit()
 
-  def test_can_visit_upload_dashboard(self):
+  def test_can_see_landing_page(self):
 
     # Edith has heard about a cool new online to-do app. She goes
     # to check out its homepage.
@@ -41,9 +42,30 @@ class NewVisitorTest(unittest.TestCase):
     # She notices the page title and header welcomes her to
     # the app and tells her its name.
     self.assertIn('Welcome - Training Zealot', self.browser.title)
+    header_text = self.browser.find_element(By.TAG_NAME, 'h2').text
+    self.assertIn('Welcome', header_text)
+    navbar_text = self.browser.find_element(
+      By.XPATH, 
+      '//nav[contains(@class, "navbar")]/a[contains(@class, "navbar-brand")]'
+    ).text
+    self.assertIn('The Training Zealot Analysis Platform', navbar_text)
+
+    # She sees links inviting her to visit a list of her Strava activities...
+    self.assertIsNotNone(
+      self.browser.find_element(By.LINK_TEXT, 'Strava activities'))
+
+    # ...a training log dashboard...
+    self.assertIsNotNone(
+      self.browser.find_element(By.LINK_TEXT, 'Training log dashboard'))
+
+    # ...and a file analysis dashboard.
+    self.assertIsNotNone(
+      self.browser.find_element(By.LINK_TEXT,
+        'Analyze an activity file (.gpx, .fit, .tcx, .csv)'))
+
     self.fail('Finish the test!')
 
-    # She clicks a link to visit a page for activity file analysis.
+    # She clicks a link to visit the page for activity file analysis.
     
     # There is a widget to upload her activity file
 
