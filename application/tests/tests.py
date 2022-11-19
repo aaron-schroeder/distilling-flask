32c06fc12b930572a10c3a9680c308442a872b06
aaron-schroeder <aaschro@gmail.com>
2022-11-19 12:47:28 -0700

    Failing unittest w/ strange behavior - If HomePageTest runs first, no problem - If HomePageTest runs second, the url map is messed up - This behavior disappears if setUpClass is used instead of setUp - Suspect it has to do with the way routes are set up and/or imported

--- application/tests/tests.py
+++ application/tests/tests.py
@@ -15,47 +15,7 @@ from application.models import Activity
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 
-class HomePageTest(unittest.TestCase):
-  
-  @classmethod
-  def setUpClass(cls):
-    """
-    Refs:
-      https://coddyschool.com/upload/Flask_Web_Development_Developing.pdf#page=221
-      https://stackoverflow.com/questions/60111814/flask-application-was-not-able-to-create-a-url-adapter-for-request
-    """
-    # cls.app = create_app('testing')
-    cls.app = create_app(test_config={
-      'TESTING': True,
-      'SQLALCHEMY_DATABASE_URI': os.environ.get(
-        'TEST_DATABASE_URL',
-        'sqlite:///' + os.path.join(BASEDIR, 'data-test.sqlite')
-      )
-    })
-    cls.app_context = cls.app.app_context()
-    cls.app_context.push()
-    cls.request_context = cls.app.test_request_context()
-    cls.request_context.push()
-    # db.create_all()  # happens in `create_app()`
-    cls.client = cls.app.test_client(use_cookies=True)
-
-  @classmethod
-  def tearDownClass(cls):
-    db.session.remove()
-    db.drop_all()
-    cls.app_context.pop()
-    cls.request_context.pop()
-
-  def test_home_page_returns_correct_html(self):
-    response = self.client.get('/')
-
-    html = response.get_data(as_text=True)
-    self.assertTrue(html.startswith('<!doctype html>'))
-    self.assertIn('<title>Welcome - Training Zealot</title>', html)
-
-
-class ActivityModelTest(unittest.TestCase):
-
+class FlaskTestCase(unittest.TestCase):
   def setUp(self):
     """
     Refs:
@@ -70,14 +30,29 @@ class ActivityModelTest(unittest.TestCase):
         'sqlite:///' + os.path.join(BASEDIR, 'data-test.sqlite')
       )
     })
-    self.client = self.app.test_client(use_cookies=True)
     self.app_context = self.app.app_context()
     self.app_context.push()
+    # db.create_all()
+    self.client = self.app.test_client(use_cookies=True)
 
   def tearDown(self):
     db.session.remove()
     db.drop_all()
     self.app_context.pop()
+
+
+class HomePageTest(FlaskTestCase):
+
+  def test_home_page_returns_correct_html(self):
+    print(self.app.url_map)
+    response = self.client.get('/')
+
+    html = response.get_data(as_text=True)
+    self.assertTrue(html.startswith('<!doctype html>'))
+    self.assertIn('<title>Welcome - Training Zealot</title>', html)
+
+
+class ActivityModelTest(FlaskTestCase):
 
   def test_saving_and_retrieving_items(self):
     first_act = Activity(
