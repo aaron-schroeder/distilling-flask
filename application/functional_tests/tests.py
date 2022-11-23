5d57abfe9d9f6b1211d86dc87420b4331a56d273
aaron-schroeder <aaschro@gmail.com>
2022-11-23 12:26:40 -0700

    Add smoke FT for css loading

--- application/functional_tests/tests.py
+++ application/functional_tests/tests.py
@@ -17,6 +17,7 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.color import Color
 from webdriver_manager.chrome import ChromeDriverManager
 
 from application import db, create_app, stravatalk
@@ -299,3 +300,20 @@ class NewVisitorTest(LiveServerTestCase):
     self.assertRegex(tss, r'^[0-9].*\.[0-9]$')
 
     self.fail('Finish the test!')
+
+  def test_layout_and_styling_smoke(self):
+    # A user visits the landing page.
+    self.browser.get(self.server_url)
+
+    # They notice the font family isn't just some plain old default...
+    title = self.browser.find_element(By.XPATH, '//header/h2')
+    self.assertNotIn(
+      'Times New Roman',
+      title.value_of_css_property('font-family').strip()
+    )
+
+    # ...and the color is a little more distinguished than plain black.
+    self.assertNotEqual(
+      Color.from_string(title.value_of_css_property('color')).hex,
+      '#000000',
+    )
