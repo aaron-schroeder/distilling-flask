3a1e594a43e19efa27baf5d90f1a9893f03d5c8b
aaron-schroeder <aaschro@gmail.com>
2022-11-26 15:20:39 -0700

    tests for stravatalk.get_activities_json

--- application/tests/test_stravatalk.py
+++ application/tests/test_stravatalk.py
@@ -83,7 +83,37 @@ class TestRefreshToken(unittest.TestCase):
 
 
 class TestGetActivities(unittest.TestCase):
-  pass
+  mock_token = 'abcdefg'
+
+  @patch('application.stravatalk.requests.get')
+  def test_no_kwargs(self, mock_get):
+    mock_json = ['activity1', 'activity2']
+    mock_get.return_value = Mock(
+      json=Mock(return_value=mock_json)
+    )
+    result = stravatalk.get_activities_json(self.mock_token)
+    self.assertEqual(result, mock_json)
+    data_arg = mock_get.call_args.kwargs['data']
+    self.assertNotIn('page', data_arg)
+    self.assertNotIn('per_page', data_arg)
+
+  @patch('application.stravatalk.requests.get')
+  def test_page(self, mock_get):
+    result = stravatalk.get_activities_json(self.mock_token, page=2)
+    data_arg = mock_get.call_args.kwargs['data']
+    self.assertEqual(data_arg['page'], 2)
+
+  @patch('application.stravatalk.requests.get')
+  def test_limit(self, mock_get):
+    result = stravatalk.get_activities_json(self.mock_token, limit=5)
+    data_arg = mock_get.call_args.kwargs['data']
+    self.assertEqual(data_arg['per_page'], 5)
+
+  @patch('application.stravatalk.requests.get')
+  def test_headers(self, mock_get):
+    result = stravatalk.get_activities_json(self.mock_token)
+    headers_arg = mock_get.call_args.kwargs['headers']
+    self.assertEqual(headers_arg['Authorization'], f'Bearer {self.mock_token}')
 
 
 class TestGetActivity(unittest.TestCase):
