e77ce2af6d44c4c44546e529a09cc13717f42d8b
aaron-schroeder <aaschro@gmail.com>
2022-12-15 16:13:02 -0700

    Add more commentary to test_dashboard_strava

--- application/plotlydash/tests/test_dashboard_strava.py
+++ application/plotlydash/tests/test_dashboard_strava.py
@@ -1,13 +1,30 @@
 import unittest
 
 
+class PagesFunctionalTest:
+  pass
+
+
 class StravaPageTest(unittest.TestCase):
   # TODO: Figure out how to test a specific dash page, typ.
+  # What I want:
+  #  - Do not touch the strava web API (use dummy data)
+  #  - Still allow me to drive around the dashboard like in FTs
+  #  - The user flows as if they already accepted strava permissions
+  # ...answer: (upcoming version of) AuthenticatedUserFunctionalTest
+  # ...which requires a refactor of how I store the users' token!
+  #    It makes no sense to save in a session value (which goes away
+  #    when the user goes to another website or closes the browser)
+  #    while providing the user the ability to save items to the DB. 
 
   def test_duplicate_activity_isnt_saved(self):
+    # Try to save a new (duplicate) activity programmatically
     # self.client.post('/lists/new', data={'item_text': ''})
-    # self.assertEqual(List.objects.count(), 0)
-    # self.assertEqual(Item.objects.count(), 0)
+
+    # Verify that the duplicate Activity was not saved to the DB.
+    # self.assertEqual(Activity.objects.count(), 1)
+
+    # ???
     # self.assertNotContains(response, 'other list item 1')
     # self.assertNotContains(response, 'other list item 2')
     pass
