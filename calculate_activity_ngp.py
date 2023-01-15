38e8f8c54ea2c0e9253e1075554ec2c6a774bfec
aaron-schroeder <aaschro@gmail.com>
2023-01-15 12:43:07 -0700

    Remove temporary ngp data migration script

--- calculate_activity_ngp.py
+++
@@ -1,8 +0,0 @@
-
-from application.models import db, Activity
-from application.util import units
-
-
-for activity in Activity.query.all():
-  activity.ngp_ms = units.pace_to_speed('6:30') * activity.intensity_factor
-  db.session.commit()
