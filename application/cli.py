90b88175c8e2aa50ca49f42497e6c03f4fd5e95b
aaron-schroeder <aaschro@gmail.com>
2023-03-14 16:04:19 -0600

    stub cli for startproject

--- application/cli.py
+++ application/cli.py
@@ -1,10 +1,35 @@
 import datetime
+import os
+import shutil
 
 import click
 
+import application as distilling_flask
 from application import create_app
 from application.models import db, Activity, StravaAccount
 from application.util import units
+
+
+class CommandError(Exception):
+  """Verbatim from Django.
+
+  Exception class indicating a problem while executing a management
+  command.
+
+  If this exception is raised during the execution of a management
+  command, it will be caught and turned into a nicely-printed error
+  message to the appropriate output stream (i.e., stderr); as a
+  result, raising this exception (with a sensible description of the
+  error) is the preferred way to indicate that something has gone
+  wrong in the execution of a command.
+
+  Ref:
+    https://github.com/django/django/blob/8a844e761d098d4005725f991a5e120a1f17cb70/django/core/management/base.py#L21
+  """
+
+  def __init__(self, *args, returncode=1, **kwargs):
+    self.returncode = returncode
+    super().__init__(*args, **kwargs)
 
 
 @click.group(
@@ -16,6 +41,77 @@ def cli():
   """Management script for the distilling-flask application."""
   pass
 
+
+@cli.command()
+@click.argument('project_name')
+def startproject(project_name):
+  """Creates a distilling-flask project directory structure
+  for the given project name in the current directory.
+  """
+  print(f'df startproject {project_name}')
+
+  top_dir = os.path.join(os.getcwd(), project_name)
+  try:
+    os.makedirs(top_dir)
+  except FileExistsError:
+    raise CommandError(f'{top_dir} already exists')
+  except OSError as e:
+    raise CommandError(e)
+  
+  base_name = 'project_name'
+
+  template_dir = os.path.join(distilling_flask.__path__[0], 'conf', 'project_template')
+  print(template_dir)
+  prefix_length = len(template_dir) + 1
+
+  for root, dirs, files in os.walk(template_dir):
+    path_rest = root[prefix_length:]
+    relative_dir = path_rest.replace(base_name, project_name)
+    if relative_dir:
+      target_dir = os.path.join(top_dir, relative_dir)
+      os.makedirs(target_dir, exist_ok=True)
+
+    for dirname in dirs[:]:
+      if dirname.startswith(".") or dirname == "__pycache__":
+        dirs.remove(dirname)
+
+    for filename in files:
+      if filename.endswith((".pyo", ".pyc", ".py.class")):
+        # Ignore some files as they cause various breakages.
+        continue
+      old_path = os.path.join(root, filename)
+      new_path = os.path.join(
+        top_dir, relative_dir, filename.replace(base_name, project_name)
+      )
+
+      for old_suffix, new_suffix in (".py-tpl", ".py"),:
+          if new_path.endswith(old_suffix):
+            new_path = new_path[:-len(old_suffix)] + new_suffix
+            break  # Only rewrite once
+
+      if os.path.exists(new_path):
+        raise CommandError(
+          f'{new_path} already exists. Overlaying a project into an existing '
+          'directory won\'t replace conflicting files.'
+        )
+      
+      # # Only render the Python files, as we don't want to
+      # # accidentally render Django templates files
+      # if new_path.endswith(extensions) or filename in extra_files:
+      #   with open(old_path, encoding="utf-8") as template_file:
+      #     content = template_file.read()
+      #   template = Engine().from_string(content)
+      #   content = template.render(context)
+      #   with open(new_path, "w", encoding="utf-8") as new_file:
+      #     new_file.write(content)
+      # else:
+      shutil.copyfile(old_path, new_path)
+
+      # if self.verbosity >= 2:
+      # self.stdout.write("Creating %s" % new_path)
+
+      # self.apply_umask(old_path, new_path)
+      # self.make_writeable(new_path)
 
 @cli.command()
 @click.option(
