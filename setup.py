f1d061dd1209f4b1e6473c6579676f505e1be086
aaron-schroeder <aaschro@gmail.com>
2023-03-21 12:32:44 -0600

    Prepare pypi packaging resources
    Switched from setup.py to pyproject.toml.
    So now setup.py is just a stub.
    
    Finished rounding out missing package info in pyproject.toml.

--- setup.py
+++ setup.py
@@ -1,25 +1,5 @@
-from setuptools import find_packages, setup
+# setup.py
 
+from setuptools import setup
 
-setup(
-  name='distilling-flask',
-  version='0.1.0-alpha.1',
-  description='TODO',
-  url='https://github.com/aaron-schroeder/distilling-flask',
-  author='Aaron Schroeder',
-  author_email='aaron@trailzealot.com',
-  license=None,
-  packages=find_packages(exclude=['tests*']),
-  include_package_data=True,
-  install_requires=[
-    'dash>=2.5.0',
-    'dash-bootstrap-components>=1.0.0',
-    'Flask>=2.1.0',
-  ],
-  entry_points={
-    'console_scripts': [
-      'df=distilling_flask.cli:cli',
-      'dfc=distilling_flask.cli:context_cli'
-    ],
-  },
-)
+setup()
